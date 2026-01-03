aebef9194e74c94508dd18eb299e611c6bbe9b79
[arielm20] <[arielmotsenyat@gmail.com]>
2026-01-02 20:21:07 -0500

    removed the dataset, just changes to preprocessing code:

--- EEGpreprocessing.py
+++ EEGpreprocessing.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import mne
+
+class EEGPreprocessor:
+    """
+    Class for pre processing EEG data for identifying frequency band features of chronic pain.
+    Extracts frequency information from raw EEG data for AD analysis
+    """
